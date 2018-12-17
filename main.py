c2bfeba3037038163c7f31bd0eec366ac92f451b
Krogul <krogul222@gmail.com>
2018-12-17 11:06:55 +0000

    Python Interactive and Transverse Momentum

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
+#%%
 import matplotlib.pyplot as plt
 import numpy as np 
 import pandas as pd 
 
 #Load data
-train = pd.read_csv('training.csv', index_col = 0)
-test = pd.read_csv('test.csv', index_col = 0)
+train = pd.read_csv('data/training.csv', index_col = 0)
+test = pd.read_csv('data/test.csv', index_col = 0)
 
 #Head of train data
 print(train.head())
@@ -12,7 +13,14 @@ print(train.head())
 #Info
 print(train.info())
 
+#Signal and backgorund
 signal = train[train.signal == 1]
 nonsignal = train[train.signal == 0]
+
+#Transverse Momentum
 plt.hist(signal.pt/1000, range = (0, 25), bins = 100, label = 'Signal', alpha = 0.7, normed = True)
-plt.hist(signal.pt/1000, range = (0, 25), bins = 100, label = 'Background', alpha = 0.7, normed = True)
+plt.hist(nonsignal.pt/1000, range = (0, 25), bins = 100, label = 'Background', alpha = 0.7, normed = True)
+plt.xlabel('Transverse Momentum')
+plt.ylabel('Normalized Fraction')
+plt.legend()
+plt.show()
