222b20b0031d5f1c0da9f37aecd8d720dbf902c2
Krogul <krogul222@gmail.com>
2018-12-17 12:07:07 +0000

    Plotting more features

--- main.py
+++ main.py
@@ -13,6 +13,9 @@ print(train.head())
 #Info
 print(train.info())
 
+#Summary
+print(train.describe())
+
 #Signal and backgorund
 signal = train[train.signal == 1]
 nonsignal = train[train.signal == 0]
@@ -22,5 +25,37 @@ plt.hist(signal.pt/1000, range = (0, 25), bins = 100, label = 'Signal', alpha = 
 plt.hist(nonsignal.pt/1000, range = (0, 25), bins = 100, label = 'Background', alpha = 0.7, normed = True)
 plt.xlabel('Transverse Momentum')
 plt.ylabel('Normalized Fraction')
+plt.legend()
+plt.show()
+
+#Dira
+plt.hist(signal.dira, range = (0.998, 1), bins = 100, label = 'Signal', alpha = 0.7, normed = True)
+plt.hist(nonsignal.dira, range = (0.998, 1), bins = 100, label = 'Background', alpha = 0.7, normed = True)
+plt.xlabel('Dira')
+plt.ylabel('Count')
+plt.legend()
+plt.show()
+
+#Flight Distance
+plt.hist(signal.FlightDistance, range = (0, 100), bins = 100, label = 'Signal', alpha = 0.7, normed = True)
+plt.hist(nonsignal.FlightDistance, range = (0, 100), bins = 100, label = 'Background', alpha = 0.7, normed = True)
+plt.xlabel('Flight Distance')
+plt.ylabel('Normalized Fraction')
+plt.legend()
+plt.show()
+
+#Impact Paraneter
+plt.hist(signal.IP, range = (0, 0.5), bins = 100, label = 'Signal', alpha = 0.7, normed = True)
+plt.hist(nonsignal.IP, range = (0, 0.5), bins = 100, label = 'Background', alpha = 0.7, normed = True)
+plt.xlabel('Impact Paraneter')
+plt.ylabel('Count')
+plt.legend()
+plt.show()
+
+#Track Isolation Variable
+plt.hist(signal.iso, range=(0,20), bins=20, label='Signal', alpha=0.7,normed=True)
+plt.hist(nonsignal.iso,range=(0,20),bins=20, label='Background',alpha=0.7,normed=True)
+plt.xlabel('Track Isolation Variable')
+plt.ylabel('Count')
 plt.legend()
 plt.show()
